7d0e98a8edeb90b612d251d419ae33b62e3ee468
Japtavya Singh <japtavyas@gmail.com>
2024-07-08 14:13:21 +0530

    Adding

---
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './NewsLetter.css'
+
+export const NewsLetter = () => {
+  return (
+    <div className='newsletter'>
+        <h1>Get Exclusive offers On Your Email</h1>
+        <p>Subscribe To Our Newsletter And Stay Updates</p>
+        <div>
+            <input type="email" name="" id="" placeholder='Your Email id' />
+            <button>Subscribe</button>
+        </div>
+    </div>
+  )
+}
